26ff156fed3e74c329ec1767ce2c3667ac90474c
joetats <tatuskojc@gmail.com>
2022-07-13 23:56:58 -0400

    add link to footer to github

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -4,6 +4,12 @@ const Footer = (props) => {
   return (
     <footer className="footer bg-dark">
       <div className="container text-center pt-2">
+        <a
+          className="link-light"
+          href="https://github.com/joetats/stock-spike-frontend"
+        >
+          I'm open source!
+        </a>
         <p className="text-white">
           Data provided for entertainment value with zero warranty. Not
           financial advice.
