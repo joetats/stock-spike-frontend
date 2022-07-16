6eb56f7be395e6b8e2c2b3789a9e66aa6073e937
joetats <tatuskojc@gmail.com>
2022-07-15 22:41:49 -0400

    add yahoo finance link to symbols - resolves #2

--- src/components/tables/EtfWatchlistRow.js
+++ src/components/tables/EtfWatchlistRow.js
@@ -1,9 +1,19 @@
 const EtfWatchlistRow = (props) => {
   const gain = `${(parseFloat(props.gain) * 100).toFixed(2)} %`;
+  const yahooLink = `https://finance.yahoo.com/quote/${props.symbol}/`;
 
   return (
     <tr>
-      <th>{props.symbol}</th>
+      <th>
+        <a
+          href={yahooLink}
+          target="_blank"
+          className="link-light"
+          rel="noreferrer"
+        >
+          {props.symbol}
+        </a>
+      </th>
       <th>{gain}</th>
     </tr>
   );
