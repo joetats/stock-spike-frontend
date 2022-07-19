8e4789c69388c8d8e6bf41ab34e97050c280d607
joetats <tatuskojc@gmail.com>
2022-07-19 17:40:10 -0400

    add dark mode light mode to footer

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,34 +1,57 @@
+import { useContext } from 'react';
+
+import ThemeContext from '../../store/theme-context';
+
 const Footer = (props) => {
   const year = new Date().getFullYear();
 
+  const ctx = useContext(ThemeContext);
+
+  const themes = {};
+
+  if (ctx.isDarkMode) {
+    themes.footer = 'footer bg-dark';
+    themes.link = 'link-light';
+    themes.p = 'text-white';
+    themes.label = 'form-check-label text-white';
+  } else {
+    themes.footer = 'footer bg-light';
+    themes.link = 'link-dark';
+    themes.p = 'text-dark';
+    themes.label = 'form-check-label text-dark';
+  }
+
   return (
-    <footer className="footer bg-dark">
+    <footer className={themes.footer}>
       <div className="container text-center pt-2">
         <a
-          className="link-light"
+          className={themes.link}
           href="https://github.com/joetats/stock-spike-frontend"
         >
           I'm open source!
         </a>
-        <p className="text-white">
+        <p className={themes.p}>
           Data provided for entertainment value with zero warranty. Not
           financial advice.
         </p>
-        <div class="form-check form-switch col-5 mx-auto">
-          <input
-            className="form-check-input"
-            type="checkbox"
-            id="flexSwitchCheckDefault"
-            onChange={props.onChangeTheme}
-          />
-          <label
-            className="form-check-label text-white"
-            for="flexSwitchCheckDefault"
-          >
-            Dark Mode!
-          </label>
+        <div className="row justify-content-center">
+          <div className="col-5 col-sm-3">
+            <div className="form-check form-switch">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                id="flexSwitchCheckDefault"
+                onChange={props.onChangeTheme}
+                checked={ctx.isDarkMode}
+              />
+              <label className={themes.label} for="flexSwitchCheckDefault">
+                Dark Mode!
+              </label>
+            </div>
+          </div>
         </div>
-        <p className="text-white">Copyright © {year} Joe Tatusko</p>
+
+        <p className={themes.p}>Copyright © {year} Joe Tatusko</p>
       </div>
     </footer>
   );
