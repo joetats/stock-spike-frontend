11bb07bf3ddba080485278ac095117e56a391313
joe tats <tatuskojc@gmail.com>
2023-07-18 22:49:59 +0000

    add grid components

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # stock-spike-frontend
 
-## The open-source frontend to [stock-spike](https://stock-spike.com).
+## The open-source frontend (and now backend!) to [stock-spike](https://stock-spike.com).
 
 Feel free to add issues or pull requests!!!
--- src/bootstrap.py
+++ src/bootstrap.py
@@ -1,3 +1,11 @@
+from dash import html
 # this is where the components will end up.
 
 ## functions should return dash html or dcc objects and apply bootstrap classes
+
+def container(children):
+    return html.Div(children=children, className="container")
+
+def row(children):
+    return html.Div(children=children, className="row")
+
